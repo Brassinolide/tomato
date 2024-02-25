bed492699bcafb2f83ba34e3e2e7c480e4df5c8f
Brassinolide <99178854+Brassinolide@users.noreply.github.com>
2024-02-25 15:01:10 +0800

    oxorany

--- oxorany/oxorany_include.h
+++ oxorany/oxorany_include.h
@@ -1,5 +1,6 @@
 #ifndef OXORANY_INCLUDE_H
 #define OXORANY_INCLUDE_H
+#ifndef DISABLE_OXORANY
 
 //disable /GL for warning C4307 in msvc
 //#define OXORANY_DISABLE_OBFUSCATION
@@ -9,14 +10,8 @@
 #include "oxorany.h"
 
 //to change Marco in a easy way
-
-#ifndef DISABLE_OXORANY
 #define WRAPPER_MARCO oxorany
 #define WRAPPER_MARCO_FLT oxorany_flt
-#else
-#define oxorany(x) x
-#define oxorany_flt(x) x
-#endif
 
 template <typename T>
 static OXORANY_FORCEINLINE void copy_string_without_return(T* target, const T* source)
@@ -31,4 +26,9 @@ static OXORANY_FORCEINLINE void copy_string_without_return(T* target, const T* s
 
 #define WRAPPER_MARCO_DEFINE_STRING_ARRAY(type,name,s) type name[sizeof(s)/sizeof(type)]; copy_string_without_return(name, WRAPPER_MARCO(s))
 
-#endif
+#else
+#define oxorany(x) x
+#define oxorany_flt(x) x
+#endif //DISABLE_OXORANY
+
+#endif //OXORANY_INCLUDE_H
